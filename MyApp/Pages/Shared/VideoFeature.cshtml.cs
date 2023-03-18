1865445436f973f6939de58efe5a37762d2a7fbc
Demis Bellot <demis.bellot@gmail.com>
2023-03-18 22:05:12 +0800

    Add Blazor Page

---
+++ MyApp/Pages/Shared/VideoFeature.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Pages.Shared;
+
+public class VideoFeature : PageModel
+{
+    public string TitleTop { get; set; }
+    public string? TitleBottom { get; set; }
+    public string? Subtitle { get; set; }
+    public string YoutubeId { get; set; }
+}
