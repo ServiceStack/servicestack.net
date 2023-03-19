7b0d5d3e5a1436c7c9fb338e57570c9089bd69e1
Demis Bellot <demis.bellot@gmail.com>
2023-03-19 21:31:33 +0800

    Add training + support pages

--- MyApp/Pages/Shared/Hero.cshtml.cs
+++ MyApp/Pages/Shared/Hero.cshtml.cs
@@ -2,13 +2,14 @@
 
 namespace MyApp.Pages.Shared;
 
-public class VideoHero : PageModel
+public class Hero : PageModel
 {
-    public string BackgroundUrl { get; set; }
+    public string BackgroundUrl { get; set; } = "/img/backgrounds/bg-header.jpg";
     public string BackgroundClass { get; set; } = "bg-opacity-80";
     public string Heading { get; set; }
-    public string SubHeading { get; set; }
+    public string? SubHeading { get; set; }
     public string? ActionUrl { get; set; }
     public string? ActionText { get; set; }
-    public string? YoutubeId { get; set; }
+    public string? Content { get; set; }
+    public string ContentStyle { get; set; } = "min-height:713px";
 }
