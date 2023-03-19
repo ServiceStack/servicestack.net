5f1c254f46428b27d57a2ec43e97854ec3b17455
Demis Bellot <demis.bellot@gmail.com>
2023-03-20 02:46:31 +0800

    Add fundamental pages

---
+++ MyApp/Pages/Shared/DocsHeader.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Pages.Shared;
+
+public class DocsHeader : PageModel
+{
+    public string Heading { get; set; }
+    public string SubHeading { get; set; }
+}
