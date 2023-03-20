82ba367575e2edef1a8524b28ead9c40ff1dddca
Demis Bellot <demis.bellot@gmail.com>
2023-03-20 15:24:19 +0800

    Update MarkdownPages.cs

--- MyApp/MarkdownPages.cs
+++ MyApp/MarkdownPages.cs
@@ -6,41 +6,6 @@ using ServiceStack.Logging;
 
 namespace MyApp;
 
-public class MarkdownPages : MarkdownPagesBase
-{
-    public MarkdownPages(ILogger<MarkdownPages> log) : base(log){}
-    public List<MarkdownFileInfo> Pages { get; set; } = new();
-    
-    public MarkdownFileInfo? GetBySlug(string slug) => Pages.FirstOrDefault(x => x.Slug == slug);
-
-    public void LoadFrom(string fromDirectory)
-    {
-        Pages.Clear();
-        var fs = AssertVirtualFiles();
-        var files = fs.GetDirectory(fromDirectory).GetAllFiles().ToList();
-        var log = LogManager.GetLogger(GetType());
-        log.InfoFormat("Found {0} pages", files.Count);
-
-        var pipeline = CreatePipeline();
-
-        foreach (var file in files)
-        {
-            try
-            {
-                var doc = Load(file.VirtualPath, pipeline);
-                if (doc == null)
-                    continue;
-
-                Pages.Add(doc);
-            }
-            catch (Exception e)
-            {
-                log.Error(e, "Couldn't load {0}: {1}", file.VirtualPath, e.Message);
-            }
-        }
-    }
-}
-
 public class Videos : MarkdownPagesBase
 {
     public Videos(ILogger<Videos> log) : base(log){}
@@ -74,6 +39,41 @@ public class Videos : MarkdownPagesBase
                 {
                     log.LogError(e, "Couldn't load {0}: {1}", file.VirtualPath, e.Message);
                 }
+            }
+        }
+    }
+}
+
+public class MarkdownPages : MarkdownPagesBase
+{
+    public MarkdownPages(ILogger<MarkdownPages> log) : base(log){}
+    public List<MarkdownFileInfo> Pages { get; set; } = new();
+    
+    public MarkdownFileInfo? GetBySlug(string slug) => Pages.FirstOrDefault(x => x.Slug == slug);
+
+    public void LoadFrom(string fromDirectory)
+    {
+        Pages.Clear();
+        var fs = AssertVirtualFiles();
+        var files = fs.GetDirectory(fromDirectory).GetAllFiles().ToList();
+        var log = LogManager.GetLogger(GetType());
+        log.InfoFormat("Found {0} pages", files.Count);
+
+        var pipeline = CreatePipeline();
+
+        foreach (var file in files)
+        {
+            try
+            {
+                var doc = Load(file.VirtualPath, pipeline);
+                if (doc == null)
+                    continue;
+
+                Pages.Add(doc);
+            }
+            catch (Exception e)
+            {
+                log.Error(e, "Couldn't load {0}: {1}", file.VirtualPath, e.Message);
             }
         }
     }
