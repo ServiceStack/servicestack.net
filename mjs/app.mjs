b3a269e33615eb4a151a8eb0721c6dafd22f91cd
GitHub Action <action@github.com>
2023-04-24 03:08:17 +0000

    deploy: ae126f45825327c12067c392f5c5d95a22813b50

--- mjs/app.mjs
+++ mjs/app.mjs
@@ -116,7 +116,6 @@ export function init(exports) {
         const res = await fetch(BaseUrl + '/stats/projects.json')
         const json = await res.json()
         AppData.installs = json.results || {}
-        console.log('AppData.installs', AppData)
     })
     mountAll()
 
