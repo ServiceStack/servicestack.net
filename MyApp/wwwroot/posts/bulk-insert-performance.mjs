0e76ad87a7ba3f86c954ac265de289267d77f416
Demis Bellot <demis.bellot@gmail.com>
2023-08-23 23:21:02 +0800

    Publish New Blogging features in Razor SSG post

--- MyApp/wwwroot/posts/bulk-insert-performance.mjs
+++ MyApp/wwwroot/posts/bulk-insert-performance.mjs
@@ -1,38 +1,5 @@
-import { ref, onMounted } from "vue"
-import { addScript } from "@servicestack/client"
-
-const addChartsJs = await addScript('../js/chart.js')
-
-export const ChartJs = {
-    template:`
-        <div>
-            <canvas ref="chart"></canvas>
-        </div>
-    `,
-    props:['type','data','options'],
-    setup(props) {
-        const chart = ref()
-        onMounted(async () => {
-            await addChartsJs
-
-            const options = props.options || {
-                responsive: true,
-                legend: {
-                    position: "top"
-                }
-            }
-            new Chart(chart.value, {
-                type: props.type || "bar",
-                data: props.data,
-                options,
-            })
-
-        })
-        return { chart }
-    }
-}
+import ChartJs from './components/ChartJs.mjs'
 
 export default {
     components: { ChartJs }
 }
-
---
+++ MyApp/wwwroot/posts/components/ChartJs.mjs
@@ -0,0 +1,29 @@
+import { ref, onMounted } from "vue"
+import { addScript } from "@servicestack/client"
+
+const loadJs = addScript('../../js/chart.js')
+
+export default {
+    template:`<div><canvas ref="chart"></canvas></div>`,
+    props:['type','data','options'],
+    setup(props) {
+        const chart = ref()
+        onMounted(async () => {
+            await loadJs
+
+            const options = props.options || {
+                responsive: true,
+                legend: {
+                    position: "top"
+                }
+            }
+            new Chart(chart.value, {
+                type: props.type || "bar",
+                data: props.data,
+                options,
+            })
+
+        })
+        return { chart }
+    }
+}
