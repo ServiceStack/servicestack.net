3fc6ddc7e096536d968df5dbd087c261639d0cc4
GitHub Action <action@github.com>
2024-11-26 06:07:54 +0000

    deploy: 502461539a8ee7f506037f98c203b620181c5632

--- posts/ai-server.mjs
+++ posts/ai-server.mjs
@@ -1,3 +1,12 @@
+import AsciiCinema from './components/AsciiCinema.mjs'
+
 export default {
-    
+    install(app) {
+    },
+    components: {
+        AsciiCinema,
+    },
+    setup() {
+        return { }
+    }
 }
