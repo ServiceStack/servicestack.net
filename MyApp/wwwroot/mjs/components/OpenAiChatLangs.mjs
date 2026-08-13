18373c35786aec0fea72dc45057d2386b4c9255f
Demis Bellot <demis.bellot@gmail.com>
2026-08-13 16:06:05 +0800

    tweaks

--- MyApp/wwwroot/mjs/components/OpenAiChatLangs.mjs
+++ MyApp/wwwroot/mjs/components/OpenAiChatLangs.mjs
@@ -542,8 +542,8 @@ export default {
                 <svg class="h-5 w-5 flex-shrink-0 text-gray-300" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                   <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
                 </svg>
-                <a href="https://docs.servicestack.net/ai-server/chat" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">
-                    About Open AI Chat API
+                <a href="https://docs.servicestack.net/chat" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">
+                    About AI Chat
                 </a>
               </div>
             </li>
